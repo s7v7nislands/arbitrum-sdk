226e95c06b4d7908581a20b2a97651a69f050210
seven <seven@nodereal.io>
2022-12-21 12:18:44 +0800

    fix

--- src/lib/utils/lib.ts
+++ src/lib/utils/lib.ts
@@ -6,13 +6,14 @@ export const wait = (ms: number): Promise<void> =>
   new Promise(res => setTimeout(res, ms))
 
 export const getBaseFee = async (provider: Provider) => {
-  const baseFee = (await provider.getBlock('latest')).baseFeePerGas
-  if (!baseFee) {
-    throw new ArbSdkError(
-      'Latest block did not contain base fee, ensure provider is connected to a network that supports EIP 1559.'
-    )
-  }
-  return baseFee
+  return 500000000
+  // const baseFee = (await provider.getBlock('latest')).baseFeePerGas
+  // if (!baseFee) {
+  //   throw new ArbSdkError(
+  //     'Latest block did not contain base fee, ensure provider is connected to a network that supports EIP 1559.'
+  //   )
+  // }
+  // return baseFee
 }
 
 /**
